2654ca9a486fb2cf4e9bda9c8dc0dc7afdb09b40
SzNeUrTo <Supanut.Overload@gmail.com>
2015-01-15 11:56:30 -0500

    callback function

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+var sync = require('synchronize');
 var mysql = require('mysql');
+var temp = '';
 var connection_db = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
@@ -6,6 +8,15 @@ var connection_db = mysql.createConnection({
 	database : 'test_with_nodejs'
 });
 connection_db.connect(function(err) {});
+
+
+var indexs = {
+	'561050xxxx':0,
+	x564050xxxx:1
+};
+
+var id_all = ['56for','67adf'];
+var points = [];
 
 //var io = require('socket.io').listen(SOCKETIO_PORT);
 //io.sockets.on('connection', function() {
@@ -18,16 +29,18 @@ function insertValue(data) {
 	var query = connection_db.query('INSERT INTO table1 SET ?', data, function(err, result) {
 		console.log(err);
 		console.log(result);
+		temp = result;
 	});
 }
 
-function getValueFormID(id) {
+function getValueFormID(id, callback) {
 	var query = connection_db.query('select * from table1 where id='+id, function(err, result) {
-		console.log(err);
-		console.log(result);
-		return result;
+		if (err) {
+			callback(err);
+		} else {
+			callback(null, result);
+		}
 	});
-	return query;
 }
 
 function updateValueAtID(id, point) {
@@ -37,8 +50,12 @@ function updateValueAtID(id, point) {
 	});
 }
 
-
-//console.log(query.sql);
-//insertValue()
-//insertValue({id:3, point:20 name:'test test2'});
-getValueFormID(3);
+id = 3
+getValueFormID(id, function(err, callback) {
+	if (err) {
+		console.error('Error!', err);
+	} else {
+		console.log(callback);
+		id = callback;
+	}
+});
